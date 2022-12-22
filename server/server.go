2715497c65cc46ece3f750eb5a3632362c7613cd
Bi11y G04t <wtran292gmail.com>
2022-12-22 14:30:57 -0800

    set up grpc binding to serve endpoints

--- server/server.go
+++ server/server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"log"
+	"mailinglist/grpcapi"
 	"mailinglist/jsonapi"
 	"mailinglist/mdb"
 	"sync"
@@ -11,23 +12,28 @@ import (
 )
 
 var args struct {
-	Dbpath   string `arg:"env:MAILINGLIST_DB`
+	DbPath   string `arg:"env:MAILINGLIST_DB`
 	BindJson string `arg:"env:MAILINGLIST_BIND_JSON`
+	BindGrpc string `arg:"env:MAILINGLIST_BIND_GRPC`
 }
 
 func main() {
 	arg.MustParse(&args)
 
-	if args.Dbpath == "" {
-		args.Dbpath = "list.db"
+	if args.DbPath == "" {
+		args.DbPath = "list.db"
 	}
 
 	if args.BindJson == "" {
 		args.BindJson = ":8080"
 	}
 
-	log.Printf("using database '%v'\n", args.Dbpath)
-	db, err := sql.Open("sqlite3", args.Dbpath)
+	if args.BindGrpc == "" {
+		args.BindGrpc = ":8081"
+	}
+
+	log.Printf("using database '%v'\n", args.DbPath)
+	db, err := sql.Open("sqlite3", args.DbPath)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -41,6 +47,14 @@ func main() {
 	go func() {
 		log.Printf("starting JSON API server...\n")
 		jsonapi.Serve(db, args.BindJson)
+		wg.Done()
+
+	}()
+
+	wg.Add(1)
+	go func() {
+		log.Printf("starting gRPC API server...\n")
+		grpcapi.Serve(db, args.BindGrpc)
 		wg.Done()
 
 	}()
